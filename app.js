a886a6bba67bc562972565364ab6b481496081e2
EC2 Default User <ec2-user@ip-172-31-24-28.ap-northeast-1.compute.internal>
2022-09-10 02:55:25 +0000

    [Finish]js5

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-$(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>');
+//window.addEventListener('DOMContentLoaded', jQueryが読み込まれる前でも影響を受けない
+//window.onload = ページ全部が読み込まれた後に実行したい
+$(function(){
+  $('.box1').slideDown(function(){
+    $('.box1').css({
+        'background-color': '#0000FF',
+        'height': '100px',
+        'width': '200px'
+    }).slideUp();
+  });
 });
