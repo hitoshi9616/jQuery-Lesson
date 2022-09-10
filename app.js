bdfa5ab04f53be96c0759f01fd9b3f16fdb554ba
EC2 Default User <ec2-user@ip-172-31-24-28.ap-northeast-1.compute.internal>
2022-09-10 03:12:38 +0000

    [Finish]js6

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 //window.addEventListener('DOMContentLoaded', jQueryが読み込まれる前でも影響を受けない
 //window.onload = ページ全部が読み込まれた後に実行したい
 $(function(){
-  $('.box1').slideDown(function(){
-    $('.box1').css({
-        'background-color': '#0000FF',
-        'height': '100px',
-        'width': '200px'
-    }).slideUp();
+  $('.box1').on('click', function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
   });
 });
